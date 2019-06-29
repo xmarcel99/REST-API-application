8c46c0ddb332ef561bd42fa0b6e86770cc5a7ae6
Marcel Sztur <sensiss99@gmail.com>
2019-06-29 18:30:13 +0200

    Exercises 21.4

--- showtasks.sh
+++ showtasks.sh
@@ -2,11 +2,11 @@
 
 openGetTasksPage()
 {
-    x-www-browser http://localhost:8080/crud/v1/task/getTasks
+    x-www-browser --no-sandbox http://localhost:8080/crud/v1/task/getTasks
 }
 errorMessage()
 {
-    echo "We can't run Tomcat server"
+    echo "Error with running your Tomcat server"
 }
 
 if ./runcrud.sh; then
