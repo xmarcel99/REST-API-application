ce8777bae18d49071221c10aee475e1942d0b83a
Marcel Sztur <sensiss99@gmail.com>
2019-07-04 11:26:56 +0200

    Exercises 22.2

--- src/main/java/com/crud/tasks/controller/NotFoundTrelloBoardException.java
+++ src/main/java/com/crud/tasks/controller/NotFoundTrelloBoardException.java
@@ -1,4 +1,4 @@
 package com.crud.tasks.controller;
 
-public class NotFoundTrelloBoardException {
+public class NotFoundTrelloBoardException extends Exception {
 }
--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,4 +1,34 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
 public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() throws NotFoundTrelloBoardException {
+
+        List<TrelloBoardDto> trelloBoards = new ArrayList<>();
+
+        for (int i = 0; i < trelloClient.getBoardsResponse().length; i++) {
+            TrelloBoardDto trelloBoardDto = trelloClient.getTrelloBoards(i).orElseThrow(NotFoundTrelloBoardException::new);
+            trelloBoards.add(trelloBoardDto);
+        }
+        trelloBoards.stream()
+                .filter(trelloBoardDto -> trelloBoardDto.getId() != null && trelloBoardDto.getName() != null && trelloBoardDto.getName().contains("Kodilla"))
+                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+    }
 }
