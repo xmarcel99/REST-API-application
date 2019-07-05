2a93fa99dc1f3a916f25107f8fd252cd8ecd0059
Marcel Sztur <sensiss99@gmail.com>
2019-07-05 16:37:58 +0200

    exercises 22.4

--- src/main/java/com/crud/tasks/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/client/TrelloClient.java
@@ -3,9 +3,10 @@ package com.crud.tasks.client;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.config.TrelloConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -13,61 +14,56 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class TrelloClient {
     @Autowired
     RestTemplate restTemplate;
 
-    @Value("${trello.api.endpoint.prod}")
-    private String trelloApiEndpoint;
+    @Autowired
+    TrelloConfig trelloConfig;
 
-    @Value("${trello.app.key}")
-    private String trelloAppKey;
-
-    @Value("${trello.app.token}")
-    private String trelloToken;
-
-    @Value("${trello.app.id}")
-    private String username;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloClient.class);
 
 
-    public URI getTrelloUrl() {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + username + "/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/members/" + trelloConfig.getUsername() + "/boards")
+                .queryParam("key",trelloConfig.getTrelloAppKey())
+                .queryParam("token", trelloConfig.getTrelloToken())
                 .queryParam("fields", "name,id")
-                .queryParam("lists","all").build().encode().toUri();
-        return url;
-    }
+                .queryParam("lists", "all").build().encode().toUri();
+        try {
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+            return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
+        } catch (RestClientException e) {
+            LOGGER.error(e.getMessage(),e);
+            return new ArrayList<>();
+        }
 
-    public List<TrelloBoardDto> getTrelloBoards(){
-          TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getTrelloUrl(), TrelloBoardDto[].class);
-          if(boardsResponse != null) {
-              return Arrays.asList(boardsResponse);
-          }
-          return new ArrayList<>();
     }
 
     public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto) {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint+"/cards")
-                .queryParam("key",trelloAppKey)
-                .queryParam("token",trelloToken)
-                .queryParam("name",trelloCardDto.getName())
-                .queryParam("desc",trelloCardDto.getDescription())
-                .queryParam("pos",trelloCardDto.getPos())
-                .queryParam("idList",trelloCardDto.getListId()).build().encode().toUri();
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/cards")
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token",  trelloConfig.getTrelloToken())
+                .queryParam("name", trelloCardDto.getName())
+                .queryParam("desc", trelloCardDto.getDescription())
+                .queryParam("pos", trelloCardDto.getPos())
+                .queryParam("idList", trelloCardDto.getListId()).build().encode().toUri();
 
-        return restTemplate.postForObject(url,null,CreatedTrelloCard.class);
+        return restTemplate.postForObject(url, null, CreatedTrelloCard.class);
     }
 
     public CreatedTrelloCard getCard(String cardId) {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint+"/cards/"+cardId)
-                .queryParam("key",trelloAppKey)
-                .queryParam("token",trelloToken)
-                .queryParam("fields","id,name,shortUrl,badges").build().encode().toUri();
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/cards/" + cardId)
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token",  trelloConfig.getTrelloToken())
+                .queryParam("fields", "id,name,shortUrl,badges").build().encode().toUri();
 
-        return restTemplate.getForObject(url,CreatedTrelloCard.class);
+        return restTemplate.getForObject(url, CreatedTrelloCard.class);
     }
 }
---
+++ src/test/java/com/crud/tasks/client/TrelloClientTest.java
@@ -0,0 +1,88 @@
+package com.crud.tasks.client;
+
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.config.TrelloConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloClientTest {
+
+    @InjectMocks
+    TrelloClient trelloClient;
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @Mock
+    TrelloConfig trelloConfig;
+    @Before
+    public void init() {
+        ReflectionTestUtils.setField(trelloConfig,"trelloApiEndpoint","http://test.com");
+        ReflectionTestUtils.setField(trelloConfig,"trelloAppKey","test");
+        ReflectionTestUtils.setField(trelloConfig,"trelloToken","test");
+        ReflectionTestUtils.setField(trelloConfig,"username","marcelsztur");
+        when(trelloConfig.getTrelloApiEndpoint()).thenReturn((String) ReflectionTestUtils.getField(trelloConfig,"trelloApiEndpoint"));
+        when(trelloConfig.getTrelloAppKey()).thenReturn((String) ReflectionTestUtils.getField(trelloConfig,"trelloAppKey"));
+        when(trelloConfig.getTrelloToken()).thenReturn((String) ReflectionTestUtils.getField(trelloConfig,"trelloToken"));
+        when(trelloConfig.getUsername()).thenReturn((String) ReflectionTestUtils.getField(trelloConfig,"username"));
+    }
+    @Test
+    public void shouldFetchTrelloBoards() throws URISyntaxException {
+        //Given
+        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
+        trelloBoards[0] = new TrelloBoardDto("test_board", "test_id", new ArrayList<>());
+
+        URI url = new URI("http://test.com/members/marcelsztur/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(url,TrelloBoardDto[].class)).thenReturn(trelloBoards);
+        //When
+        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
+        //Then
+        assertEquals(1,fetchedTrelloBoards.size());
+        assertEquals("test_id",fetchedTrelloBoards.get(0).getId());
+        assertEquals("test_board",fetchedTrelloBoards.get(0).getName());
+        assertEquals(new ArrayList<>(),fetchedTrelloBoards.get(0).getLists());
+    }
+//
+//    @Test
+//    public void shouldCreateCard() throws URISyntaxException{
+//        //Given
+//        URI uri = new URI("http://test.com/cards?key=test&token=test&name=Test%20task&desc=Test%20Description&pos=top&idList=test_id");
+//
+//        CreatedTrelloCard createdTrelloCard = new CreatedTrelloCard(
+//                "1",
+//                "Test task",
+//                "http://test.com"
+//        );
+//
+//        TrelloCardDto trelloCardDto = new TrelloCardDto(
+//                "Test task",
+//                "Test Description",
+//                "top",
+//                "idddddd"
+//        );
+//        //When
+//        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+//        //Then
+//
+//
+//    }
+
+}
